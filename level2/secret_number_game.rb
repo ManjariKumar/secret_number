0da5aef85b3b949b0307648d97a17022c3cda77e
ManjariKumar <manjari.kumar.ny@gmail.com>
2014-05-19 14:38:59 -0400

    Secret NUmber Game Level 2 - Running well. Haven't tested with rspec yet

--- level2/secret_number_game.rb
+++ level2/secret_number_game.rb
@@ -19,19 +19,20 @@
 class SecretNumberGame
   
   def initialize
-    @first_name = ___
-    @last_name = ___
-    @secret_number = ___
-    @guesses = ___
+    @first_name = "Joe"
+    @last_name = "Leo"
+    @secret_number = rand(10)+1
+    @guesses = 0
   end
 
   def play
     greeting
     
     @guesses.times do |x|
-      guesses_left = ___ #Find a way to decrement guesses_left each time through the loop.
-      puts "\nYou have #{guesses_left} guesses left!"
-      puts "Please make your first guess:"
+      #Find a way to decrement guesses_left each time through the loop.
+      puts "\nYou have #{@guesses} guesses left!"
+      @guesses -= 1 
+      puts "Please make your guess:"
       players_guess = $stdin.gets.chomp.to_i
       return if correct?(players_guess)
     end
@@ -39,29 +40,54 @@ class SecretNumberGame
   end
 
   def ask_difficulty
-    ___ # As the user if they want to play on easy (1), medium (2), or hard (3)
+    # As the user if they want to play on easy (1), medium (2), or hard (3)
+    print "Choose your level of difficulty:\n 1. Easy \n 2. Medium \n 3. Hard \n"
+    user_choice_string = $stdin.gets.chomp!
+    user_choice_num = user_choice_string.to_i 
+    if ((user_choice_num == 1) || (user_choice_num == 2) || (user_choice_num == 3))
+      difficulty_level = user_choice_num
+    elsif user_choice_string.capitalize == "Easy"
+      difficulty_level = 1
+    elsif user_choice_string.capitalize == "Medium"
+      difficulty_level = 2
+    elsif user_choice_string.capitalize == "Hard"
+        difficulty_level = 3
+    else ask_difficulty
+    end
+    set_difficulty(difficulty_level)                    
   end
 
   def set_difficulty(level)
-    ___ # limit the number of guesses based on the difficulty
+    # limit the number of guesses based on the difficulty
+    if level == 1
+      @guesses=5
+    elsif level == 2
+      @guesses=3
+    elsif level == 3
+      @guesses=1
+    end
+  
   end
     
   def greet(player_name)
-    ___ #Create a string to use as your greeting. It should include the player_name and the rules, with the number of guesses the player gets.
+    #Create a string to use as your greeting. It should include the player_name and the rules, with the number of guesses the player gets.
+    player_name.capitalize!
+    print "\nHi #{player_name} ! Secret Number is a guessing game. \nThe purpose of the game is to have players guess a secret number from 1-10. \nDepending on the level of difficulty you choose, you'll get 1, 3 or 5 guesses to find the secret number. \n" 
   end
 
   def created_by
-    ___ #Created_by is a string that includes your first and last name
+    #Created_by is a string that includes your first and last name
+    print "\nCreated by #{@first_name} #{@last_name} \n"
   end
 
 
   def correct?(players_guess)
-    if ___
+    if players_guess == @secret_number
       puts "You win! The number was #{@secret_number}."
       return true
-    elsif ___
+    elsif ((players_guess < @secret_number) && (@guesses > 0))
       puts "Make your next guess higher than #{players_guess}"
-    elsif ___
+    elsif ((players_guess > @secret_number) && (@guesses > 0))
       puts "Make your next guess lower than #{players_guess}"
     end
     false
@@ -71,11 +97,13 @@ class SecretNumberGame
   private 
 
   def greeting
+    puts "\nSECRET NUMBER GAME\n"
+    puts "~~~~~~~~~~~~~~~~~~\n"
     puts created_by
-    ask_difficulty
     puts "\nWhat is your name?"
     player_name = $stdin.gets.chomp
     puts greet(player_name)
+    ask_difficulty
   end
 
   def ___(*arguments)
